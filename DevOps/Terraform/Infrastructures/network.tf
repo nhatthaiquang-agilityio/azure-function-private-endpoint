d76b198eabc50e637178249f0d9a6815d6aff2bd
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2024-11-26 14:21:48 +0700

    Add delegation

--- DevOps/Terraform/Infrastructures/network.tf
+++ DevOps/Terraform/Infrastructures/network.tf
@@ -18,6 +18,20 @@ resource "azurerm_subnet" "az_func_subnet" {
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.az_func_network.name
   address_prefixes     = ["10.0.2.0/24"]
+
+  dynamic "delegation" {
+    for_each = lookup(each.value, "delegation", [])
+    content {
+      name = lookup(delegation.value, "name", null)
+      dynamic "service_delegation" {
+        for_each = lookup(delegation.value, "service_delegation", [])
+        content {
+          name    = lookup(service_delegation.value, "name", null)    # (Required) The name of service to delegate to. Possible values include Microsoft.BareMetal/AzureVMware, Microsoft.BareMetal/CrayServers, Microsoft.Batch/batchAccounts, Microsoft.ContainerInstance/containerGroups, Microsoft.Databricks/workspaces, Microsoft.HardwareSecurityModules/dedicatedHSMs, Microsoft.Logic/integrationServiceEnvironments, Microsoft.Netapp/volumes, Microsoft.ServiceFabricMesh/networks, Microsoft.Sql/managedInstances, Microsoft.Sql/servers, Microsoft.Web/hostingEnvironments and Microsoft.Web/serverFarms.
+          actions = lookup(service_delegation.value, "actions", null) # (Required) A list of Actions which should be delegated. Possible values include Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action, Microsoft.Network/virtualNetworks/subnets/action and Microsoft.Network/virtualNetworks/subnets/join/action.
+        }
+      }
+    }
+  }
 }
 
 # Create Network Interface
